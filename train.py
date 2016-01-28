d3963fef0173c9c20ccb1b7f4ec32d9e194661da
Miquel Perello Nieto <mp15688@it025739.users.bris.ac.uk>
2016-01-28 14:29:42 +0000

    Organized all the code

--- train.py
+++ train.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 plt.ion()
 plt.rcParams['image.cmap'] = 'gray'
+plt.rcParams['figure.figsize'] = (6,4)
 
 
 np.random.seed(1234)
@@ -13,8 +14,8 @@ from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation, Flatten
 from keras.layers.convolutional import Convolution2D, MaxPooling2D
 from keras.regularizers import l2, activity_l2
-from keras.optimizers import SGD, Adadelta, Adagrad, Adamax
-from keras.utils.visualize_util import plot
+from keras.optimizers import SGD, Adadelta, Adagrad, Adamax, RMSprop
+#from keras.utils.visualize_util import plot
 
 from keras.utils import np_utils
 from keras.datasets import mnist
@@ -23,12 +24,13 @@ PATH_SAVE='datasets/mnist/'
 
 #shape='spirals'
 #optimizer = SGD(lr=0.5, decay=1e-1, momentum=0.9, nesterov=True)
-optimizer = Adadelta(lr=1.0, rho=0.95, epsilon=1e-06)
+#optimizer = Adadelta(lr=1.0, rho=0.95, epsilon=1e-06)
+optimizer = RMSprop()
 #optimizer = Adagrad(lr=1.0, epsilon=1e-06)
 #optimizer = Adamax(lr=0.002, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
 num_epochs=30
 batch_size=100
-loss='binary_crossentropy'
+loss='categorical_crossentropy'
 
 def create_cnn():
     model = Sequential()
@@ -65,110 +67,132 @@ def create_mlp(num_out=10):
     model.add(Activation('relu'))
     model.add(Dropout(0.2))
     model.add(Dense(num_out))
-    model.add(Activation('sigmoid'))
+    model.add(Activation('softmax'))
     return model
 
-model = create_mlp(num_out=10)
-plot(model, to_file='model.png')
-model.compile(optimizer=optimizer, loss=loss)
-
-def reliability_diagram(prob, Y):
+def reliability_diagram(prob, Y, label=''):
     hist_tot = np.histogram(prob, bins=np.linspace(0,1,11))
     hist_pos = np.histogram(prob[Y == 1], bins=np.linspace(0,1,11))
     plt.plot([0,1],[0,1], 'r--')
     plt.plot(hist_pos[1][:-1]+0.05, np.true_divide(hist_pos[0],hist_tot[0]+1),
-             'bx-', linewidth=2.0)
+             'x-', linewidth=2.0, label=label)
+
+def plot_reliability_diagram(prob_train, Y_train, prob_test, Y_test, epoch,
+                             save=True):
+    fig = plt.figure('reliability_diagram')
+    plt.clf()
+    plt.title('Reliability diagram')
+    reliability_diagram(prob_train, Y_train, label='train')
+    reliability_diagram(prob_test, Y_test, label='test')
+    plt.legend(loc='lower right')
+    plt.grid(True)
+    plt.show()
+    if save:
+        plt.savefig('rel_dia_{:03}.svg'.format(epoch))
 
 def compute_accuracy(scores, labels, threshold=0.5):
     return np.mean((scores >= threshold) == labels)
 
-(X_train, y_train), (X_test, y_test) = mnist.load_data()
-#X_train /= 255
-#X_test /= 255
-X_train = binaryze_dataset(X_train)
-X_train = add_salt_and_pepper(X_train,proportion=0.25)
+def preprocess_data(X,y,nb_classes=10, binarize=False, seasoning=False):
+    X = X.reshape(-1, 784)
+    X = X.astype('float32')
+    X /= 255.0
+    print(X.shape[0], 'samples')
+    Y = np_utils.to_categorical(y,nb_classes)
+    if binarize:
+        X = binaryze_dataset(X, threshold=0.5)
+    if seasoning:
+        X = add_salt_and_pepper(X,proportion=0.10)
+    return X,Y
 
-X_train = X_train.reshape(60000, 784)
-X_test = X_test.reshape(10000, 784)
-X_train = X_train.astype('float32')
-X_test = X_test.astype('float32')
-
-Y_train = np_utils.to_categorical(y_train, nb_classes)
-Y_test = np_utils.to_categorical(y_test, nb_classes)
-
-##X = np.load(path.join(PATH_SAVE, "training_X.npy"))
-###X = np.reshape(X, (np.shape(X)[0],1,28,28))
-##X = np.reshape(X, (np.shape(X)[0],-1))
-##Y = np.load(path.join(PATH_SAVE, "training_Y.npy"))
-##
-### FIXME : I am reducing the size of the dataset to test the code
-##X = X[0:10000]
-##Y = Y[0:10000]
-##Y = np.in1d(Y,[0,2,4,6,8])
-###Y[Y!=5] = 0
-###Y[Y==5] = 1
-
-error = np.zeros(num_epochs+1)
-accuracy = np.zeros(num_epochs+1)
-score = model.evaluate(X_train, Y_train, batch_size=batch_size, show_accuracy=True)
-error[0] = score[0]
-accuracy[0] = score[1]
-for epoch in range(1,num_epochs+1):
-    #X_train, Y_train = generate_samples(shape=shape, samples=samples)
-    model.fit(X_train, Y_train, nb_epoch=1, batch_size=batch_size,
-              show_accuracy=True)
-
-    score = model.evaluate(X_train, Y_train, batch_size=batch_size, show_accuracy=True)
-    error[epoch] = score[0]
-    print("EPOCH {}, error = {}".format(epoch, error[epoch]))
-
-    prob = model.predict(X_train)
-    fig = plt.figure('reliability_diagram')
-    plt.clf()
-    plt.title('Reliability diagram')
-    reliability_diagram(prob, Y_train)
+def imshow_samples(X_train, y_train, X_test, y_test, num_samples=4, save=True):
+    fig = plt.figure('samples')
+    for j, (data_x, data_y) in enumerate([(X_train, y_train), (X_test, y_test)]):
+        for i in range(num_samples):
+            plt.subplot(2,num_samples,(j*num_samples+i)+1)
+            plt.imshow(np.reshape(data_x[i], (28,28)))
+            plt.title(data_y[i])
     plt.show()
-    plt.savefig('rel_dia_{:03}.svg'.format(epoch))
+    if save:
+        plt.savefig('samples.svg')
 
-    fig = plt.figure('histogram_scores')
-    plt.clf()
-    plt.title('Histogram of scores')
-    plt.hist(prob)
-    plt.show()
-    plt.savefig('hist_scor_{:03}.svg'.format(epoch))
-
-    accuracy[epoch] = score[1]
+def plot_accuracy(accuracy_train, accuracy_test, epoch, save=True):
     fig = plt.figure('accuracy')
     plt.clf()
-    plt.title('Accuracy')
-    plt.plot(range(0,epoch), accuracy[:epoch])
+    plt.title("Accuracy")
+    plt.plot(range(0,epoch), accuracy_train[:epoch], label='train')
+    plt.plot(range(0,epoch), accuracy_test[:epoch], label='test')
+    plt.legend(loc='lower right')
     plt.show()
-    plt.savefig('accuracy_{:03}.svg'.format(epoch))
+    if save:
+        plt.savefig('accuracy_{:03}.svg'.format(epoch))
 
-    fig = plt.figure('error', figsize=(6,4))
+def plot_error(error_train, error_test, epoch, loss, save=True):
+    fig = plt.figure('error')
     plt.clf()
     plt.title(loss)
-    plt.plot(range(0,epoch), error[:epoch])
+    plt.plot(range(0,epoch), error_train[:epoch], label='train')
+    plt.plot(range(0,epoch), error_test[:epoch], label='test')
+    plt.legend()
     plt.ylabel(loss)
     plt.xlabel('epoch')
     plt.show()
-    plt.savefig('{}_{:03}.svg'.format(loss, epoch))
+    if save:
+        plt.savefig('{}_{:03}.svg'.format(loss, epoch))
+
+def plot_histogram_scores(scores, epoch, save=True):
+    fig = plt.figure('histogram_scores')
+    plt.clf()
+    plt.title('Histogram of scores (train)')
+    plt.hist(scores)
+    plt.show()
+    if save:
+        plt.savefig('hist_scor_{:03}.svg'.format(epoch))
+
+nb_classes=10
+model = create_mlp(num_out=nb_classes)
+#plot(model, to_file='model.png')
+model.compile(optimizer=optimizer, loss=loss)
+
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+X_train, Y_train = preprocess_data(X_train, y_train, nb_classes=nb_classes)
+X_test, Y_test = preprocess_data(X_test, y_test, nb_classes=nb_classes)
+
+imshow_samples(X_train, y_train, X_test, y_test, 5)
+
+error_train  = np.zeros(num_epochs+1)
+error_test = np.zeros(num_epochs+1)
+accuracy_train = np.zeros(num_epochs+1)
+accuracy_test = np.zeros(num_epochs+1)
+score = model.evaluate(X_train, Y_train, batch_size=batch_size, show_accuracy=True)
+error_train[0] = score[0]
+accuracy_train[0] = score[1]
+score = model.evaluate(X_test, Y_test, batch_size=batch_size, show_accuracy=True)
+error_test[0] = score[0]
+accuracy_test[0] = score[1]
+for epoch in range(1,num_epochs+1):
+    history = model.fit(X_train, Y_train, nb_epoch=1, batch_size=batch_size,
+                        show_accuracy=True)
+
+    # TODO use the history instead of evaluate again the training set
+    score_train = model.evaluate(X_train, Y_train, batch_size=batch_size, show_accuracy=True)
+    score_test = model.evaluate(X_test, Y_test, batch_size=batch_size, show_accuracy=True)
+    error_train[epoch] = score_train[0]
+    error_test[epoch] = score_test[0]
+    print("EPOCH {}, train error = {}, test error = {}".format(epoch, error_train[epoch], error_test[epoch]))
+
+
+    prob_train = model.predict(X_train)
+    prob_test = model.predict(X_test)
+    plot_reliability_diagram(prob_train, Y_train, prob_test, Y_test, epoch)
+
+    plot_histogram_scores(prob_train, epoch)
+
+    accuracy_train[epoch] = score_train[1]
+    accuracy_test[epoch] = score_test[1]
+    plot_accuracy(accuracy_train, accuracy_test, epoch)
+
+    plot_error(error_train, error_test, epoch, loss)
 
     plt.pause(0.1)
-
-fig = plt.figure('error', figsize=(6,4))
-plt.clf()
-plt.title(loss)
-plt.plot(range(0,num_epochs+1), error[0:])
-plt.ylabel(loss)
-plt.xlabel('epoch')
-plt.show()
-plt.savefig('{}.svg'.format(loss))
-
-accuracy[epoch] = compute_accuracy(prob, Y_train)
-fig = plt.figure('accuracy')
-plt.clf()
-plt.title("Accuracy")
-plt.plot(range(0,epoch), accuracy[:epoch])
-plt.show()
-plt.savefig('accuracy_{:03}.svg'.format(epoch))
