6db6dfbfc83c4e81ef5538109e280f873d30c792
perellonieto <perello.nieto@gmail.com>
2016-02-12 18:09:10 +0000

    Adapting code for Theano. With this commit nothing works, get the previous version as a working commit

--- train.py
+++ train.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python
+import os
+import sys
+import timeit
+
 from os import path
 from scripts.create_dataset import binaryze_dataset, add_salt_and_pepper
 import numpy as np
+import numpy
 from sklearn.isotonic import IsotonicRegression
+from data import load_data
 
+import theano
+import theano.tensor as T
+from mlp import MLP
+
+# FIXME the code is not working with TensorFlow, the error loss is not computed
+# correctly
 from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation, Flatten
 from keras.layers.convolutional import Convolution2D, MaxPooling2D
@@ -35,15 +47,15 @@ PATH_SAVE='datasets/mnist/'
 binarize=True
 add_noise=True
 
-optimizer = SGD(lr=0.5, decay=1e-1, momentum=0.9, nesterov=True)
-#optimizer = Adadelta(lr=1.0, rho=0.95, epsilon=1e-06)
+#optimizer = SGD(lr=0.5, decay=1e-1, momentum=0.9, nesterov=False)
+optimizer = Adadelta(lr=1.0, rho=0.95, epsilon=1e-06)
 #optimizer = RMSprop()
 #optimizer = Adagrad(lr=1.0, epsilon=1e-06)
 #optimizer = Adamax(lr=0.002, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
 train_size=50000
 num_epochs=30
-batch_size=50000
-inner_batch_size=100
+batch_size=5000
+inner_batch_size=5000
 nb_classes=2
 noise_proportion=0.25
 score_lin=np.linspace(0,1,100)
@@ -65,11 +77,39 @@ diary.add_entry('hyperparameters', ['minibatch_method', minibatch_method])
 diary.add_entry('hyperparameters', ['output_activation', output_activation])
 diary.add_entry('hyperparameters', ['loss', loss])
 diary.add_entry('hyperparameters', ['optimizer', optimizer.get_config()['name']])
+for key, value in optimizer.get_config().iteritems():
+    diary.add_entry('hyperparameters', [key, value])
 diary.add_entry('hyperparameters', ['binarize', binarize])
 diary.add_entry('hyperparameters', ['add_noise', add_noise])
 diary.add_entry('hyperparameters', ['noise', noise_proportion])
 diary.add_notebook('training')
 diary.add_notebook('validation')
+
+def get_mnist_data(train_size, binarize, add_noise, noise_proportion,
+        test=False):
+    print('Loading MNIST dataset')
+    (X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+    print('Splitting data into training and validation')
+    X_val = np.copy(X_train[train_size:])
+    y_val = np.copy(y_train[train_size:])
+    X_train = np.copy(X_train[:train_size])
+    y_train = np.copy(y_train[:train_size])
+    print('Training shape = {}, Validation shape = {}, Test shape = {}'.format(
+        X_train.shape, X_val.shape, X_test.shape))
+
+    print('Preprocessing data: classes = {}, binarize = {}, add noise = {}'.format(
+           nb_classes, binarize, add_noise))
+    X_train, Y_train = preprocess_data(X_train, y_train, nb_classes=nb_classes,
+            binarize=binarize, noise=add_noise, proportion=noise_proportion)
+    X_val, Y_val = preprocess_data(X_val, y_val, nb_classes=nb_classes,
+            binarize=binarize, noise=add_noise, proportion=noise_proportion)
+    if test:
+        X_test, Y_test = preprocess_data(X_test, y_test, nb_classes=nb_classes,
+            binarize=binarize, noise=add_noise, proportion=noise_proportion)
+        return (X_train, Y_train), (X_val, Y_val), (X_test, Y_test)
+    else:
+        return (X_train, Y_train), (X_val, Y_val)
 
 def get_minibatch_id(total_size, batch_size, method='random', iteration=0):
     if method == 'random':
@@ -130,6 +170,15 @@ def create_mlp(num_out=10, activation='sigmoid'):
     model.add(Activation(activation))
     return model
 
+def create_model(classes, optimizer, loss):
+    if nb_classes == 2:
+        model = create_mlp(num_out=1, activation='sigmoid')
+        model.compile(optimizer=optimizer, loss=loss, class_mode='binary')
+    else:
+        model = create_mlp(num_out=nb_classes, activation='softmax')
+        model.compile(optimizer=optimizer, loss=loss)
+    return model
+
 def reliability_diagram(prob, Y, marker='--', label=''):
     # TODO modify the centers of the bins by their centroids
     hist_tot = np.histogram(prob, bins=np.linspace(0,1,11))
@@ -170,9 +219,9 @@ def preprocess_data(X,y,nb_classes=10, binarize=False, noise=False,
         Y = np_utils.to_categorical(y,nb_classes)
     return X_new,Y
 
-def imshow_samples(X_train, y_train, X_val, y_val, num_samples=4):
+def imshow_samples(X_train, Y_train, X_val, Y_val, num_samples=4):
     fig = plt.figure('samples')
-    for j, (data_x, data_y) in enumerate([(X_train, y_train), (X_val, y_val)]):
+    for j, (data_x, data_y) in enumerate([(X_train, Y_train), (X_val, Y_val)]):
         for i in range(num_samples):
             plt.subplot(2,num_samples,(j*num_samples+i)+1)
             plt.imshow(np.reshape(data_x[i], (28,28)))
@@ -213,37 +262,18 @@ def isotonic_gradients(ir, scores, delta=0.01):
 #    return (upper_value-lower_value)/delta*2
     return -np.ones(np.shape(scores))
 
+
 def main():
-    if nb_classes == 2:
-        model = create_mlp(num_out=1, activation='sigmoid')
-        model.compile(optimizer=optimizer, loss=loss, class_mode='binary')
-    else:
-        model = create_mlp(num_out=nb_classes, activation='softmax')
-        model.compile(optimizer=optimizer, loss=loss)
+    model = create_model(nb_classes, optimizer, loss)
 
     if keras_plot_available:
         plot(model, to_file='model.png')
 
-    print('Loading MNIST dataset')
-    (X_train, y_train), (X_test, y_test) = mnist.load_data()
-
-    print('Splitting data into training and validation')
-    X_val = np.copy(X_train[train_size:])
-    y_val = np.copy(y_train[train_size:])
-    X_train = np.copy(X_train[:train_size])
-    y_train = np.copy(y_train[:train_size])
-    print('Training shape = {}, Validation shape = {}, Test shape = {}'.format(
-        X_train.shape, X_val.shape, X_test.shape))
-
-    print('Preprocessing data: classes = {}, binarize = {}, add noise = {}'.format(
-           nb_classes, binarize, add_noise))
-    X_train, Y_train = preprocess_data(X_train, y_train, nb_classes=nb_classes,
-            binarize=binarize, noise=add_noise, proportion=noise_proportion)
-    X_val, Y_val = preprocess_data(X_val, y_val, nb_classes=nb_classes,
-            binarize=binarize, noise=add_noise, proportion=noise_proportion)
+    (X_train, Y_train), (X_val, Y_val) = get_mnist_data(
+            train_size, binarize, add_noise, noise_proportion, test=False)
 
     print('Showing data samples')
-    imshow_samples(X_train, y_train, X_val, y_val, 5)
+    imshow_samples(X_train, Y_train, X_val, Y_val, 5)
     diary.save_figure(plt, filename='samples', extension='svg')
 
     print('Creating error and accuracy vectors')
@@ -311,6 +341,7 @@ def main():
                 #   c. Compute the gradients of IR
                 g_prob_train_mb = isotonic_gradients(ir, prob_train_mb)
                 #   c. Compute new values for the labels
+                #Y_train_mb_new = prob_train_mb + Y_train_mb
                 Y_train_mb_new = prob_train_mb + \
                                  np.divide(np.multiply(prob_train_mb - Y_train_mb,
                                                        g_prob_train_mb),
@@ -320,6 +351,8 @@ def main():
                 Y_train_mb_new = Y_train_mb
 
             # 3. Train the network on this minibatch
+            #    Be advised that the errors shown by Keras on the training
+            #    set are really for this minibatch.
             print('\tTRAINING MODEL')
             model.fit(X_train_mb, Y_train_mb_new, nb_epoch=1,
                     batch_size=inner_batch_size, show_accuracy=True, verbose=1,
@@ -334,7 +367,7 @@ def main():
                 print('\tLearning Isotonic Regression from TRAINING set')
                 ir.fit(score_train, Y_train)
 
-        # Evaluate epoch
+        # Evaluate epoch on the full training and validation set
         # 5. Evaluate the performance with the calibrated probabilities
         print('\tModel predict training scores')
         score_train = model.predict(X_train).flatten()
@@ -382,6 +415,213 @@ def main():
         diary.save_figure(plt, filename='error', extension='svg')
         plt.pause(0.0001)
 
+def test_mlp(learning_rate=0.01, L1_reg=0.00, L2_reg=0.0001, n_epochs=1000,
+             dataset='mnist.pkl.gz', batch_size=20, n_hidden=500):
+    """
+    Demonstrate stochastic gradient descent optimization for a multilayer
+    perceptron
+
+    This is demonstrated on MNIST.
+
+    :type learning_rate: float
+    :param learning_rate: learning rate used (factor for the stochastic
+    gradient
+
+    :type L1_reg: float
+    :param L1_reg: L1-norm's weight when added to the cost (see
+    regularization)
+
+    :type L2_reg: float
+    :param L2_reg: L2-norm's weight when added to the cost (see
+    regularization)
+
+    :type n_epochs: int
+    :param n_epochs: maximal number of epochs to run the optimizer
+
+    :type dataset: string
+    :param dataset: the path of the MNIST dataset file from
+                 http://www.iro.umontreal.ca/~lisa/deep/data/mnist/mnist.pkl.gz
+
+
+   """
+    datasets = load_data(dataset)
+
+    train_set_x, train_set_y = datasets[0]
+    valid_set_x, valid_set_y = datasets[1]
+    test_set_x, test_set_y = datasets[2]
+
+    # compute number of minibatches for training, validation and testing
+    n_train_batches = train_set_x.get_value(borrow=True).shape[0] // batch_size
+    n_valid_batches = valid_set_x.get_value(borrow=True).shape[0] // batch_size
+    n_test_batches = test_set_x.get_value(borrow=True).shape[0] // batch_size
+
+    ######################
+    # BUILD ACTUAL MODEL #
+    ######################
+    print('... building the model')
+
+    # allocate symbolic variables for the data
+    index = T.lscalar()  # index to a [mini]batch
+    x = T.matrix('x')  # the data is presented as rasterized images
+    y = T.ivector('y')  # the labels are presented as 1D vector of
+                        # [int] labels
+
+    rng = numpy.random.RandomState(1234)
+
+    # construct the MLP class
+    classifier = MLP(
+        rng=rng,
+        input=x,
+        n_in=28 * 28,
+        n_hidden=n_hidden,
+        n_out=10
+    )
+
+    # start-snippet-4
+    # the cost we minimize during training is the negative log likelihood of
+    # the model plus the regularization terms (L1 and L2); cost is expressed
+    # here symbolically
+    cost = (
+        classifier.negative_log_likelihood(y)
+        + L1_reg * classifier.L1
+        + L2_reg * classifier.L2_sqr
+    )
+    # end-snippet-4
+
+    # compiling a Theano function that computes the mistakes that are made
+    # by the model on a minibatch
+    test_model = theano.function(
+        inputs=[index],
+        outputs=classifier.errors(y),
+        givens={
+            x: test_set_x[index * batch_size:(index + 1) * batch_size],
+            y: test_set_y[index * batch_size:(index + 1) * batch_size]
+        }
+    )
+
+    validate_model = theano.function(
+        inputs=[index],
+        outputs=classifier.errors(y),
+        givens={
+            x: valid_set_x[index * batch_size:(index + 1) * batch_size],
+            y: valid_set_y[index * batch_size:(index + 1) * batch_size]
+        }
+    )
+
+    # start-snippet-5
+    # compute the gradient of cost with respect to theta (sotred in params)
+    # the resulting gradients will be stored in a list gparams
+    gparams = [T.grad(cost, param) for param in classifier.params]
+
+    # specify how to update the parameters of the model as a list of
+    # (variable, update expression) pairs
+
+    # given two lists of the same length, A = [a1, a2, a3, a4] and
+    # B = [b1, b2, b3, b4], zip generates a list C of same size, where each
+    # element is a pair formed from the two lists :
+    #    C = [(a1, b1), (a2, b2), (a3, b3), (a4, b4)]
+    updates = [
+        (param, param - learning_rate * gparam)
+        for param, gparam in zip(classifier.params, gparams)
+    ]
+
+    # compiling a Theano function `train_model` that returns the cost, but
+    # in the same time updates the parameter of the model based on the rules
+    # defined in `updates`
+    train_model = theano.function(
+        inputs=[index],
+        outputs=cost,
+        updates=updates,
+        givens={
+            x: train_set_x[index * batch_size: (index + 1) * batch_size],
+            y: train_set_y[index * batch_size: (index + 1) * batch_size]
+        }
+    )
+    # end-snippet-5
+
+    ###############
+    # TRAIN MODEL #
+    ###############
+    print('... training')
+
+    # early-stopping parameters
+    patience = 10000  # look as this many examples regardless
+    patience_increase = 2  # wait this much longer when a new best is
+                           # found
+    improvement_threshold = 0.995  # a relative improvement of this much is
+                                   # considered significant
+    validation_frequency = min(n_train_batches, patience // 2)
+                                  # go through this many
+                                  # minibatche before checking the network
+                                  # on the validation set; in this case we
+                                  # check every epoch
+
+    best_validation_loss = numpy.inf
+    best_iter = 0
+    test_score = 0.
+    start_time = timeit.default_timer()
+
+    epoch = 0
+    done_looping = False
+
+    while (epoch < n_epochs) and (not done_looping):
+        epoch = epoch + 1
+        for minibatch_index in range(n_train_batches):
+
+            minibatch_avg_cost = train_model(minibatch_index)
+            # iteration number
+            iter = (epoch - 1) * n_train_batches + minibatch_index
+
+            if (iter + 1) % validation_frequency == 0:
+                # compute zero-one loss on validation set
+                validation_losses = [validate_model(i) for i
+                                     in range(n_valid_batches)]
+                this_validation_loss = numpy.mean(validation_losses)
+
+                print(
+                    'epoch %i, minibatch %i/%i, validation error %f %%' %
+                    (
+                        epoch,
+                        minibatch_index + 1,
+                        n_train_batches,
+                        this_validation_loss * 100.
+                    )
+                )
+
+                # if we got the best validation score until now
+                if this_validation_loss < best_validation_loss:
+                    #improve patience if loss improvement is good enough
+                    if (
+                        this_validation_loss < best_validation_loss *
+                        improvement_threshold
+                    ):
+                        patience = max(patience, iter * patience_increase)
+
+                    best_validation_loss = this_validation_loss
+                    best_iter = iter
+
+                    # test it on the test set
+                    test_losses = [test_model(i) for i
+                                   in range(n_test_batches)]
+                    test_score = numpy.mean(test_losses)
+
+                    print(('     epoch %i, minibatch %i/%i, test error of '
+                           'best model %f %%') %
+                          (epoch, minibatch_index + 1, n_train_batches,
+                           test_score * 100.))
+
+            if patience <= iter:
+                done_looping = True
+                break
+
+    end_time = timeit.default_timer()
+    print(('Optimization complete. Best validation score of %f %% '
+           'obtained at iteration %i, with test performance %f %%') %
+          (best_validation_loss * 100., best_iter + 1, test_score * 100.))
+    #print(('The code for file ' + os.path.split(__file__)[1] + ' ran for %.2fm' % ((end_time - start_time) / 60.)), file=sys.stderr)
+
 if __name__ == '__main__':
-    status = main()
-    sys.exit(status)
+    test_mlp()
+    #status = main()
+    #sys.exit(status)
+
