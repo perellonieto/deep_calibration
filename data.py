41135a9e609fa81eff10d0af3fd061964e51a4ef
Miquel Perelló Nieto <perello.nieto@gmail.com>
2016-02-24 15:41:28 +0000

    Added generation of dataset with multiple classes with c shape, two different orientations and arbitrary center of mass

--- data.py
+++ data.py
@@ -117,6 +117,31 @@ def load_data(dataset, nb_classes=10, binarize=False, noise_prop=None):
             (test_set_x, test_set_y)]
     return rval
 
+def my_shared_dataset(data_xy, borrow=True):
+    """ Function that loads the dataset into shared variables
+
+    The reason we store our dataset in shared variables is to allow
+    Theano to copy it into the GPU memory (when code is run on GPU).
+    Since copying data into the GPU is slow, copying a minibatch everytime
+    is needed (the default behaviour if the data is not in a shared
+    variable) would lead to a large decrease in performance.
+    """
+    data_x, data_y = data_xy
+    shared_x = theano.shared(numpy.asarray(data_x,
+                                           dtype=theano.config.floatX),
+                             borrow=borrow)
+    shared_y = theano.shared(numpy.asarray(data_y,
+                                           dtype=theano.config.floatX),
+                             borrow=borrow)
+    # When storing data on the GPU it has to be stored as floats
+    # therefore we will store the labels as ``floatX`` as well
+    # (``shared_y`` does exactly that). But during our computations
+    # we need them as ints (we use labels as index, and if they are
+    # floats it doesn't make sense) therefore instead of returning
+    # ``shared_y`` we will have to cast it to int. This little hack
+    # lets ous get around this issue
+    return shared_x, T.cast(shared_y, 'int32')
+
 def generate_gaussian_data(means=[[0,0]], cov=[[[1,0],[0,1]]], samples=[1],
                            prop_train=0.5, prop_valid=0.4, prop_test=0.1):
     X = numpy.empty(shape=(sum(samples),2))
@@ -140,34 +165,46 @@ def generate_gaussian_data(means=[[0,0]], cov=[[[1,0],[0,1]]], samples=[1],
     valid_set = (X[valid_indices,:], Y[valid_indices])
     test_set = (X[test_indices,:], Y[test_indices])
 
-    def shared_dataset(data_xy, borrow=True):
-        """ Function that loads the dataset into shared variables
 
-        The reason we store our dataset in shared variables is to allow
-        Theano to copy it into the GPU memory (when code is run on GPU).
-        Since copying data into the GPU is slow, copying a minibatch everytime
-        is needed (the default behaviour if the data is not in a shared
-        variable) would lead to a large decrease in performance.
-        """
-        data_x, data_y = data_xy
-        shared_x = theano.shared(numpy.asarray(data_x,
-                                               dtype=theano.config.floatX),
-                                 borrow=borrow)
-        shared_y = theano.shared(numpy.asarray(data_y,
-                                               dtype=theano.config.floatX),
-                                 borrow=borrow)
-        # When storing data on the GPU it has to be stored as floats
-        # therefore we will store the labels as ``floatX`` as well
-        # (``shared_y`` does exactly that). But during our computations
-        # we need them as ints (we use labels as index, and if they are
-        # floats it doesn't make sense) therefore instead of returning
-        # ``shared_y`` we will have to cast it to int. This little hack
-        # lets ous get around this issue
-        return shared_x, T.cast(shared_y, 'int32')
+    test_set_x, test_set_y = my_shared_dataset(test_set)
+    valid_set_x, valid_set_y = my_shared_dataset(valid_set)
+    train_set_x, train_set_y = my_shared_dataset(train_set)
 
-    test_set_x, test_set_y = shared_dataset(test_set)
-    valid_set_x, valid_set_y = shared_dataset(valid_set)
-    train_set_x, train_set_y = shared_dataset(train_set)
+    rval = [(train_set_x, train_set_y), (valid_set_x, valid_set_y),
+            (test_set_x, test_set_y)]
+    return rval
+
+def generate_opposite_cs_data(samples=[1,1], means=[[0.45,0.75],[-0.45,-0.75]],
+                              direction=[-1,1], prop_train=0.5, prop_valid=0.4,
+                              prop_test=0.1):
+    X = numpy.empty(shape=(sum(samples),2))
+    Y = numpy.empty(shape=(sum(samples)))
+    cumsum = 0
+    for i in range(len(means)):
+        new_cumsum = cumsum + samples[i]
+        X[cumsum:new_cumsum,0] = numpy.random.uniform(-1,1,samples[i]) \
+                                 + means[i][0]
+        X[cumsum:new_cumsum,1] = (numpy.power(X[cumsum:new_cumsum,0]
+                                              - means[i][0],2)
+                                 + numpy.random.normal(0,0.1,samples[i])
+                                 )*direction[i] + means[i][1]
+        Y[cumsum:new_cumsum] = i
+        cumsum = new_cumsum
+
+    rand_indices = numpy.random.permutation(range(cumsum))
+    X = X[rand_indices,:]
+    Y = Y[rand_indices]
+    train_indices = numpy.arange(int(cumsum*prop_train))
+    valid_indices = numpy.arange(int(cumsum*prop_valid)) + train_indices[-1]+1
+    test_indices = numpy.arange(int(cumsum*prop_test)) + valid_indices[-1]+1
+
+    train_set = (X[train_indices,:], Y[train_indices])
+    valid_set = (X[valid_indices,:], Y[valid_indices])
+    test_set = (X[test_indices,:], Y[test_indices])
+
+    test_set_x, test_set_y = my_shared_dataset(test_set)
+    valid_set_x, valid_set_y = my_shared_dataset(valid_set)
+    train_set_x, train_set_y = my_shared_dataset(train_set)
 
     rval = [(train_set_x, train_set_y), (valid_set_x, valid_set_y),
             (test_set_x, test_set_y)]
